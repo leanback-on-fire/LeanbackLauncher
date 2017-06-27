f4052b6baa4407d8208500b8c7d678a97c9709a6
GaoFei <fly.gao@rock-chips.com>
2017-06-27 17:46:44 +0800

    Move Android.mk AndroidManifirst.xml to root directory.
    Signed-off-by: GaoFei <fly.gao@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -4,8 +4,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := GoogleLeanbackLauncher
 LOCAL_CERTIFICATE := platform
 LOCAL_DEX_PREOPT := false
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res \
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res \
        $(TOP)/frameworks/support/v7/preference/res \
        frameworks/support/v17/leanback/res \
        frameworks/support/v7/recyclerview/res \
