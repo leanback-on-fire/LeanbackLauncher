9e293d5a7d04d7dd05dbc2cbb8096900d269c6c8
nikk gitanes <tsynik@gmail.com>
2019-09-02 19:49:47 +0300

    custom banners update

--- app/src/main/java/com/rockon999/android/firetv/leanbacklauncher/util/BannerUtil.java
+++ app/src/main/java/com/rockon999/android/firetv/leanbacklauncher/util/BannerUtil.java
@@ -13,5 +13,12 @@ public class BannerUtil {
         BANNER_OVERRIDES.put("hulu", R.drawable.banner_hulu);
         BANNER_OVERRIDES.put("bueller.music", R.drawable.banner_amazon_music);
         BANNER_OVERRIDES.put("showtime", R.drawable.banner_showtime);
+        BANNER_OVERRIDES.put("com.amazon.avod", R.drawable.banner_amazon_prime);
+        BANNER_OVERRIDES.put("tunein.player", R.drawable.banner_tunein);
+        BANNER_OVERRIDES.put("org.mozilla.tv.firefox", R.drawable.banner_firefox);
+        BANNER_OVERRIDES.put("com.google.android.youtube.tv", R.drawable.banner_youtube);
+        BANNER_OVERRIDES.put("ru.yourok.torrserve", R.drawable.banner_torrserve);
+        BANNER_OVERRIDES.put("com.niklabs.pp", R.drawable.banner_pp);
+        BANNER_OVERRIDES.put("ru.cn.tv", R.drawable.banner_peerstv);
     }
 }
