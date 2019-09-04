dc8c23a518ece69957c13198bf6332d80311d670
nikk gitanes <tsynik@gmail.com>
2019-09-04 23:27:02 +0300

    refactor row preferences

--- app/src/main/java/com/rockon999/android/leanbacklauncher/settings/LegacyAppRowPreferenceFragment.java
+++ app/src/main/java/com/rockon999/android/leanbacklauncher/settings/LegacyAppRowPreferenceFragment.java
@@ -27,6 +27,9 @@ import java.util.Set;
 
 public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
 
+    private static final int ACTION_ID_RECOMENDATIONS = 101;
+    private static final int ACTION_ID_INPUTS = 201;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
@@ -48,7 +51,7 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
         long id = action.getId();
         long modId = (id - 1) / 4;
         int subId = (int) ((id - 1) % 4);
-        int val = 0;
+        int val = 1;
         boolean enabled = false;
         
         Activity activity = getActivity();
@@ -70,13 +73,19 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
                     RowPreferences.setFavoritesEnabled(activity, !enabled);
                     break;
                 case 2:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setFavoriteRowMax", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setFavoriteRowMax(activity, val);
                 	break;
                 case 3:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setFavoriteRowMin", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setFavoriteRowMin(activity, val);
             }
         } else if (modId == musicIndex) {
@@ -89,13 +98,19 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
                     RowPreferences.setMusicEnabled(activity, !enabled);
                     break;
                 case 2:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMax", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMax(AppCategory.MUSIC, activity, val);
                 	break;
                 case 3:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMin", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMin(AppCategory.MUSIC, activity, val);
             }
         } else if (modId == videoIndex) {
@@ -108,13 +123,19 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
                     RowPreferences.setVideosEnabled(activity, !enabled);
                     break;
                 case 2:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMax", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMax(AppCategory.VIDEO, activity, val);
                 	break;
                 case 3:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMin", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMin(AppCategory.VIDEO, activity, val);
             }
         } else if (modId == gameIndex) {
@@ -127,13 +148,19 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
                     RowPreferences.setGamesEnabled(activity, !enabled);
                     break;
                 case 2:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMax", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMax(AppCategory.GAME, activity, val);
                 	break;
                 case 3:
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setRowMin", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setRowMin(AppCategory.GAME, activity, val);
             }
         } else if (modId == allIndex) {
@@ -141,52 +168,53 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
                 case 0:
                     break;
                 case 1:
-                    // Log.w("+++ apps.rows.max", ""+action.getDescription());
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setAllAppsMax", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setAllAppsMax(activity, val);
                 	break;
                 case 2:
-                    // Log.w("+++ apps.rows.min", ""+action.getDescription());
-                    val = Integer.parseInt(action.getDescription().toString());
-                    Log.w("+++ setAllAppsMin", "" + val);
+                    try {
+                        val = Integer.parseInt(action.getDescription().toString());
+                    } catch (NumberFormatException nfe) {
+                        val = 1;
+                    }
                 	RowPreferences.setAllAppsMin(activity, val);
             }
         } else if (id == 101) { // RECOMENDATIONS
             enabled = RowPreferences.areRecommendationsEnabled(activity);
             // Log.w("+++ recommendations.enabled", ""+enabled);
             RowPreferences.setRecommendationsEnabled(activity, !enabled);
-        } else if (id == 201) { // INPUTS
+        } else if (id == ACTION_ID_INPUTS) { // INPUTS
             enabled = RowPreferences.areInputsEnabled(activity);
             // Log.w("+++ inputs.enabled", ""+enabled);
             RowPreferences.setInputsEnabled(activity, !enabled);
         }
         updateActions();
     }
-    
-//    @Override
-//    public long onGuidedActionEditedAndProceed(GuidedAction action) {
-//    
-//        return GuidedAction.ACTION_ID_NEXT;
-//    }
 
     private void updateActions() {
 
         Log.w("+++ updateActions()", "+++");
     
         ArrayList<GuidedAction> actions = new ArrayList<>();
-        boolean z = true;
         Activity activity = getActivity();
         Set<AppCategory> categories = RowPreferences.getEnabledCategories(activity);
         int i = 0;
 
-        // RECCOMENDATIONS
+        // RECOMENDATIONS
+        boolean state = RowPreferences.areRecommendationsEnabled(activity);
+        String statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
         actions.add(new GuidedAction.Builder(activity).id(100).title(R.string.recs_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(101).checkSetId(101).checked(RowPreferences.areRecommendationsEnabled(activity)).title(R.string.enable_recs_row_title).description("").build());
+        actions.add(new GuidedAction.Builder(activity).id(ACTION_ID_RECOMENDATIONS).checkSetId(ACTION_ID_RECOMENDATIONS).checked(state).title(statelabel).description("").build());
         
         // FAV
         actions.add(new GuidedAction.Builder(activity).id(++i).title(R.string.favorites_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(RowPreferences.areFavoritesEnabled(activity)).title(R.string.enable_favorites_row_title).description("").build());
+        state = RowPreferences.areFavoritesEnabled(activity);
+        statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
+        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(state).title(statelabel).description("").build());
 
         int[] constraints = RowPreferences.getFavoriteRowConstraints(activity);
 
@@ -197,7 +225,9 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
 
         // MUSIC
         actions.add(new GuidedAction.Builder(activity).id(++i).title(R.string.music_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(categories.contains(AppCategory.MUSIC)).title(R.string.enable_music_row_title).description("").build());
+        state = categories.contains(AppCategory.MUSIC);
+        statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
+        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(state).title(statelabel).description("").build());
 
         constraints = RowPreferences.getRowConstraints(AppCategory.MUSIC, activity);
 
@@ -208,7 +238,9 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
 
         // VIDEO
         actions.add(new GuidedAction.Builder(activity).id(++i).title(R.string.videos_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(categories.contains(AppCategory.VIDEO)).title(R.string.enable_videos_row_title).description("").build());
+        state = categories.contains(AppCategory.VIDEO);
+        statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
+        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(state).title(statelabel).description("").build());
 
         constraints = RowPreferences.getRowConstraints(AppCategory.VIDEO, activity);
 
@@ -219,7 +251,9 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
 
         // GAME
         actions.add(new GuidedAction.Builder(activity).id(++i).title(R.string.games_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(categories.contains(AppCategory.GAME)).title(R.string.enable_games_row_title).description("").build());
+        state = categories.contains(AppCategory.GAME);
+        statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
+        actions.add(new GuidedAction.Builder(activity).id(++i).checkSetId(i).checked(state).title(statelabel).description("").build());
 
         constraints = RowPreferences.getRowConstraints(AppCategory.GAME, activity);
 
@@ -240,7 +274,9 @@ public class LegacyAppRowPreferenceFragment extends GuidedStepSupportFragment {
         
         // INPUTS
         actions.add(new GuidedAction.Builder(activity).id(200).title(R.string.inputs_row_title).build());
-        actions.add(new GuidedAction.Builder(activity).id(201).checkSetId(201).checked(RowPreferences.areInputsEnabled(activity)).title(R.string.enable_inputs_row_title).description("").build());
+        state = RowPreferences.areInputsEnabled(activity);
+        statelabel = (state) ? getString(R.string.v7_preference_on) : getString(R.string.v7_preference_off);
+        actions.add(new GuidedAction.Builder(activity).id(ACTION_ID_INPUTS).checkSetId(ACTION_ID_INPUTS).checked(state).title(statelabel).description("").build());
         
         setActions(actions); // APPLY
     }
