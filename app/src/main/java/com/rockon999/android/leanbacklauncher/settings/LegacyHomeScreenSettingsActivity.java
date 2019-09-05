a735eaec816c555006b38d7a3e26b2c666ae6179
nikk gitanes <tsynik@gmail.com>
2019-09-05 11:35:22 +0300

    Handle Back on Launcher Settings

--- app/src/main/java/com/rockon999/android/leanbacklauncher/settings/LegacyHomeScreenSettingsActivity.java
+++ app/src/main/java/com/rockon999/android/leanbacklauncher/settings/LegacyHomeScreenSettingsActivity.java
@@ -1,12 +1,15 @@
 package com.rockon999.android.leanbacklauncher.settings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v17.leanback.app.GuidedStepFragment;
 import android.support.v17.leanback.app.GuidedStepSupportFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.res.ResourcesCompat;
 
+import com.rockon999.android.leanbacklauncher.MainActivity;
+import com.rockon999.android.leanbacklauncher.R;
 import com.rockon999.android.leanbacklauncher.R;
 
 public class LegacyHomeScreenSettingsActivity extends FragmentActivity {
@@ -16,6 +19,21 @@ public class LegacyHomeScreenSettingsActivity extends FragmentActivity {
 
         if (savedInstanceState == null) {
             GuidedStepSupportFragment.addAsRoot(this, new LegacyHomeScreenPreferenceFragment(), 16908290);
+        }
+    }   
+    @Override
+    public void onBackPressed() {
+
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            // super.onBackPressed();
+            // back to Launcher
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            getSupportFragmentManager().popBackStack();
         }
     }
 }
