0d1b9c32f06479f1c8dd953b5e77aee08e7ef6da
nikk gitanes <tsynik@gmail.com>
2020-07-21 18:32:26 +0300

    add imdb.tv
    and removed amazon news shortcut (broken)

--- app/src/main/java/com/amazon/tv/leanbacklauncher/apps/LaunchPointListGenerator.java
+++ app/src/main/java/com/amazon/tv/leanbacklauncher/apps/LaunchPointListGenerator.java
@@ -145,8 +145,9 @@ public class LaunchPointListGenerator {
                         // any system app that isn't TV-optimized likely isn't something the user needs or wants [except for Amazon Music & Photos (which apparently don't get leanback launchers :\)]
                         if (!Util.isSystemApp(LaunchPointListGenerator.this.mContext, itemRawLaunchPoint.activityInfo.packageName) ||
                             itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.bueller") ||
-                            itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.venezia") // ||
-                            // itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.hedwig")
+                            itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.venezia") ||
+                            itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.imdb.tv") &&
+                            !itemRawLaunchPoint.activityInfo.packageName.startsWith("com.amazon.hedwig") // broken launchpoint
                             ) {
                             if (!rawComponents.containsKey(itemRawLaunchPoint.activityInfo.packageName) &&
                                 !itemRawLaunchPoint.activityInfo.packageName.equals(LaunchPointListGenerator.this.mContext.getPackageName())
