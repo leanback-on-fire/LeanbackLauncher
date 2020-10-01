b994eb8d1ec029ec91c9dbc9c84614f6bbd9d08c
OTSUKAKK <62287542+OTSUKAKK@users.noreply.github.com>
2020-10-01 21:11:17 +0800

    Update AppCategorizer.java

--- app/src/main/java/com/amazon/tv/firetv/leanbacklauncher/util/AppCategorizer.java
+++ app/src/main/java/com/amazon/tv/firetv/leanbacklauncher/util/AppCategorizer.java
@@ -64,6 +64,7 @@ public class AppCategorizer {
             "com.vgtrk",
             "courville.nova",
             "ctcmediagroup.ctc",
+            "dtv.tvx",
             "dailymotion",
             "disney",
             "divantv",
