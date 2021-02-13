853079388189a22855d2a6cb887a8eddcd246aa4
nikk gitanes <tsynik@gmail.com>
2021-02-13 15:36:03 +0300

    improve wallpaper chooser

--- app/src/main/java/com/amazon/tv/leanbacklauncher/settings/LegacyFileListFragment.kt
+++ app/src/main/java/com/amazon/tv/leanbacklauncher/settings/LegacyFileListFragment.kt
@@ -16,6 +16,7 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.leanback.app.GuidedStepSupportFragment
 import androidx.leanback.widget.GuidanceStylist.Guidance
 import androidx.leanback.widget.GuidedAction
+import com.amazon.tv.leanbacklauncher.BuildConfig
 import com.amazon.tv.leanbacklauncher.MainActivity
 import com.amazon.tv.leanbacklauncher.R
 import java.io.File
@@ -23,33 +24,40 @@ import java.io.File
 class LegacyFileListFragment : GuidedStepSupportFragment() {
 
     override fun onCreateGuidance(savedInstanceState: Bundle?): Guidance {
-        val activity: Activity? = activity
-        val desc = getWallpaper(activity)
         return Guidance(
                 getString(R.string.select_wallpaper_action_title),  // title
-                desc,  // description
+                getWallpaperDesc(requireContext()),  // description
                 getString(R.string.settings_dialog_title),  // breadcrumb (parent)
                 ResourcesCompat.getDrawable(resources, R.drawable.ic_settings_home, null) // icon
         )
     }
 
     override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
-        val activity: Activity? = activity
-        if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        val activity = requireActivity()
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             Log.v(TAG, "READ_EXTERNAL_STORAGE permission is granted")
         } else {
             Log.v(TAG, "READ_EXTERNAL_STORAGE permission not granted")
-            makeRequest()
+            makeRequest(activity)
         }
-        actions.add(GuidedAction.Builder(
-                getActivity())
-                .id(ACTION_SELECT.toLong())
-                .title(String.format(getString(R.string.select), Environment.getExternalStorageDirectory()))
-                .description(null)
-                .build()
-        )
-        actions.add(GuidedAction.Builder(
-                getActivity())
+
+//        val gpath = Environment.getExternalStorageDirectory().absolutePath
+//        val spath = "Pictures"
+//        var fullpath = File(gpath + File.separator + spath)
+        val dir = File(Environment.getExternalStorageDirectory().absolutePath)
+        val images = imageReader(dir)
+
+        if (images.size > 0)
+            images.forEach {
+                actions.add(GuidedAction.Builder(activity)
+                        .id(ACTION_SELECT.toLong())
+                        .title(it.name)
+                        .description(null)
+                        .build()
+                )
+            }
+
+        actions.add(GuidedAction.Builder(activity)
                 .id(ACTION_BACK.toLong())
                 .title(R.string.goback)
                 .description(null)
@@ -57,18 +65,40 @@ class LegacyFileListFragment : GuidedStepSupportFragment() {
         )
     }
 
-    protected fun makeRequest() {
-        val activity: Activity? = activity
-        ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+    fun imageReader(root: File): ArrayList<File> {
+        val fileList: ArrayList<File> = ArrayList()
+        val listAllFiles = root.listFiles()
+
+        if (listAllFiles != null && listAllFiles.isNotEmpty()) {
+            for (currentFile in listAllFiles) {
+                if (currentFile.name.endsWith(".jpeg") || currentFile.name.endsWith(".jpg") || currentFile.name.endsWith(".png")) {
+                    // File absolute path
+                    if (BuildConfig.DEBUG) Log.d("downloadFilePath", currentFile.getAbsolutePath())
+                    // File Name
+                    if (BuildConfig.DEBUG) Log.d("downloadFileName", currentFile.getName())
+                    fileList.add(currentFile.absoluteFile)
+                }
+            }
+            if (BuildConfig.DEBUG) Log.w("fileList", "" + fileList.size)
+        }
+        return fileList
+    }
+
+    protected fun makeRequest(activity: Activity) {
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                 500)
     }
 
     override fun onGuidedActionClicked(action: GuidedAction) {
-        val activity: Activity? = activity
+        val context = requireContext()
         when (action.id.toInt()) {
             ACTION_SELECT -> {
-                val file = File(Environment.getExternalStorageDirectory(), "background.jpg")
-                if (file.canRead()) setWallpaper(activity, Environment.getExternalStorageDirectory().toString() + "/background.jpg") else Toast.makeText(activity, activity!!.getString(R.string.file_no_access), Toast.LENGTH_LONG).show()
+                val name = action.title
+                val file = File(Environment.getExternalStorageDirectory(), name.toString())
+                if (file.canRead())
+                    setWallpaper(context, file.path.toString())
+                else
+                    Toast.makeText(context, activity!!.getString(R.string.file_no_access), Toast.LENGTH_LONG).show()
                 fragmentManager!!.popBackStack()
             }
             ACTION_BACK -> fragmentManager!!.popBackStack()
@@ -77,27 +107,24 @@ class LegacyFileListFragment : GuidedStepSupportFragment() {
         }
     }
 
-    fun setWallpaper(context: Context?, image: String): Boolean {
+    private fun setWallpaper(context: Context?, image: String): Boolean {
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         if (!image.isEmpty()) pref.edit().putString("wallpaper_image", image).apply()
         // refresh home
-        val activity: Activity? = activity
+        val activity = requireActivity()
         val Broadcast = Intent(MainActivity::class.java.name) // ACTION
         Broadcast.putExtra("RefreshHome", true)
-        activity!!.sendBroadcast(Broadcast)
+        activity.sendBroadcast(Broadcast)
         return true
     }
 
-    private fun getWallpaper(context: Context?): String? {
+    private fun getWallpaperDesc(context: Context?): String? {
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         val image = pref.getString("wallpaper_image", "")
-        return if (!image!!.isEmpty()) {
+        return if (image!!.isNotBlank()) {
             image
         } else {
-            val file = File(context!!.filesDir, "background.jpg")
-            if (file.canRead()) {
-                file.toString()
-            } else null
+            getString(R.string.wallpaper_choose)
         }
     }
 
